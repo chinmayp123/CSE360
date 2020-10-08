dc789db14278a784375bd243113dd9a1521fb2f2
chinmaypulapa <chinmaypulapa@192.168.8.124>
2020-10-07 19:49:42 -0700

    Final Version(2.0)

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -2,7 +2,7 @@
  * The AddingMachine program implements an application that
  * simply adds, subtracts and finds the total for the operation.
  * 
- * @author   Chinmay Pulaa
+ * @author   Chinmay Pulapa
  * @version  2.0
  * @since    2020-10-07
  */
@@ -13,32 +13,55 @@ public class AddingMachine {
   private String history = "0";	// Private String vairbale history
   
   public AddingMachine () {
-	  // AddingMachine function 
     total = 0;  // not needed - included for clarity
   }
+  /**
+   * This method gets the total
+   * @return NOTHING
+   */
   
   public int getTotal () {
-	  //Get Total function
     return total;
   }
   
+  /**
+   * This method is the add function
+   * @param value
+   * @return Total + value
+   */
+  
   public void add (int value) {
-	  // add function, adds given value to total
 	   total=value+total;
 	   history = " + " + value;
   }
-
+  
+  /**
+   * This method is the subtract function
+   * @param value 
+   * @return Total - value
+   */
+ 
   public void subtract (int value) {
 	  // subtract function, subtracts given value from the total
 	  total=total-value;
 	  history = " - " + value;
   }
 
+  /** 
+   * This method will show us the final calculation by displaying all the operations
+   * @return history
+   */
+  
   public String toString () {
 	  //to String give the return statement of the following operation that where made
     return history;
   }
 
+  /**
+   * This method will clear all the calculations which make total and history equal to 0.
+   * @return history, total
+   */
+  
   public void clear() {
 	  //clears the data
 	  total = 0;
